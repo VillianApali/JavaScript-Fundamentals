5395511c8bb1588ee15d17787525cc6defa5e474
VillianApali <villianapali@gmail.com>
2022-10-15 06:44:30 -0700

    first commit

---
+++ literals.js
@@ -0,0 +1,19 @@
+`I counted ${10+32} sheeps`;
+console.log(`I counted ${10+32} sheeps`);
+let firstName= "villian";
+let lastName= "apali";
+// let fullName = firstName + lastName;
+// console.log(fullName);
+let fullName = `${firstName}  ${lastName}`;
+console.log(fullName)
+let total1 = 500;
+let total2 = 800;
+// let total = `${total1} + ${total2}` ;
+// console.log(total)
+let total = `ksh.${total1 + total2}`;
+console.log(total)
+let item= 'cucumbers';
+let price= 1.99;
+let quantity= 4;
+`You bought ${quantity} ${item}, totalprice: ${price*quantity}`;
+console.log(`You bought ${quantity} ${item}, totalprice: ${price*quantity}`);
---
+++ strings.js
@@ -0,0 +1,25 @@
+let annoyingLaugh = 'teehee so funny! teehee';
+annoyingLaugh.replace('teehee', 'haha')
+console.log(annoyingLaugh);
+console.log(annoyingLaugh.replace('teehee', 'haha'));
+const age= "5"+"4";
+console.log(age);
+"pecan pie"[7]
+console.log("pecan pie"[7]);
+annoyingLaugh.replaceAll('teehee', 'haha')
+console.log(annoyingLaugh.replaceAll('teehee', 'haha'));
+// new line creation
+console.log("hello\nworld")
+// adding quotes to a string or paragraph
+console.log("hello\'world")
+console.log("hello\"world\"")
+console.log("hello \\world")
+console.log("hello \rworld")
+console.log("hello \vworld")
+console.log("hello \tworld")
+console.log("hello \bworld"
+
+
+
+
+
